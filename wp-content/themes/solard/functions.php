292046c22f669dbac9544ef93104b05158c12c5b
Will Potter <will@wpotter.me>
2012-05-13 17:20:36 -0400

    added semi working team page

--- wp-content/themes/solard/functions.php
+++ wp-content/themes/solard/functions.php
@@ -19,14 +19,11 @@ function analytics() {
 <?php 
 }
 
-
-
 /**
  * Pulls the source for a header image depending on the page name. Also supports a default image with no name.
  *
  */
 function getHeaderImage($pagename) {
-
 	switch($pagename) {
 		case "team":
 			$headerImage = "";
@@ -41,10 +38,32 @@ function getHeaderImage($pagename) {
 		
 			break;
 	}
-
 }
 
+/**
+ *
+ *
+ */
+$people = array();
+$people[] = array('name'=>'Ally Silberkleit',	'team'=>'Design',		'path'=>get_template_directory_uri().'/headshots/asilberkleit-small.png');
+$people[] = array('name'=>'Bree Baccaglini',	'team'=>'Construction',		'path'=>get_template_directory_uri().'/headshots/bbaccaglini-small.png');
+$people[] = array('name'=>'Danny Nigh',			'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/dnigh-small.png');
+$people[] = array('name'=>'Ethan Galiette',		'team'=>'Finance',		'path'=>get_template_directory_uri().'/headshots/egaliette-small.png');
+$people[] = array('name'=>'Eliza Margolin',		'team'=>'Design',		'path'=>get_template_directory_uri().'/headshots/emargolin-small.png');
+$people[] = array('name'=>'Emily Roach',		'team'=>'Design',		'path'=>get_template_directory_uri().'/headshots/eroach-small.png');
+$people[] = array('name'=>'Evan Gallagher',		'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/evang-small.png');
+$people[] = array('name'=>'Jordan Collins',		'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/jcollins-small.png');
+$people[] = array('name'=>'Jillian Mock',		'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/jmock-small.png');
+$people[] = array('name'=>'Maddy Lawler',		'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/mlawler-small.png');
+$people[] = array('name'=>'Mollie Young',		'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/myoung-small.png');
+$people[] = array('name'=>'Patrick Li',			'team'=>'Design',		'path'=>get_template_directory_uri().'/headshots/pli-small.png');
+$people[] = array('name'=>'Rita Croce',			'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/rcroce-small.png');
+$people[] = array('name'=>'Becky Wasserman',	'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/rwasserman-small.png');
+$people[] = array('name'=>'Sofia Silverglass',	'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/ssilverglass-small.png');
+$people[] = array('name'=>'Sean Willerford',	'team'=>'Outreach',		'path'=>get_template_directory_uri().'/headshots/swillerford-small.png');
 
-
-
+$faculty = array();
+$faculty[] = array('name'=>'Andrea Murray', 	'role'=>'Faculty',		'path'=>get_template_directory_uri().'/headshots/akmurray-small.png');
+$faculty[] = array('name'=>'Ashar Nelson', 		'role'=>'Faculty',		'path'=>get_template_directory_uri().'/headshots/jnelson-small.png');
+$faculty[] = array('name'=>'Sarah Franco',	 	'role'=>'Administration',		'path'=>get_template_directory_uri().'/headshots/sfranco-small.png');
 ?>
--- wp-content/themes/solard/page-team.php
+++ wp-content/themes/solard/page-team.php
@@ -14,15 +14,35 @@
 <div id="content_background">
 	<div id="container">
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 
-		<div id="primary">
+		<div id="primary" class="full-page">
+			<div id="team-photos">
+			<?php foreach($people as $p) { ?>
+			<a class="team-member">
+				<img class="team-photo" src="<?php echo $p['path']; ?>" alt="" />
+				<div class="team-text">
+					<span class="team-name"><?php echo $p['name']; ?></span>
+					<span class="team-subteam"><?php echo $p['team']; ?></span>
+				</div>
+			</a>
+			<?php } ?>
+			<?php foreach($faculty as $p) { ?>
+			<a class="team-member">
+				<img class="team-photo" src="<?php echo $p['path']; ?>" alt="" />
+				<div class="team-text">
+					<span class="team-name"><?php echo $p['name']; ?></span>
+					<span class="team-subteam"><?php echo $p['role']; ?></span>
+				</div>
+			</a>
+			<?php } ?>
+
+			<div class="clear"></div>
+			</div>
 			<div id="content" role="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
-
 					<?php the_content(); ?>
-
 				<?php endwhile; // end of the loop. ?>
 
 			</div><!-- #content -->
